9674073e6cface72153928e4d2c255fc03f4e8ba
Aditya Raj <161936660+devwithaditya@users.noreply.github.com>
2025-08-01 20:03:11 +0000

    String

---
+++ basic/string.js
@@ -0,0 +1,10 @@
+let firstName = "Aditya";
+let roll = 52;
+let lastName = "raj";
+
+console.log(`Hello, My name is ${firstName} and my roll is ${roll} and last name is ${lastName}`);
+
+const username = new String("menonduty");
+let position = 4;
+console.log(`The character at position ${position} is ${username.charAt(position)}`);
+console.log(`The character at position ${position} is ${username.at(position)}`);
